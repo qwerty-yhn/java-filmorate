86cf99e0092fe691d64fbdd69f27ff41ad05f8cd
qwerty-yhn <109685954+qwerty-yhn@users.noreply.github.com>
2023-01-31 10:29:17 +0600

    Update DirectorNotFoundException.java

--- src/main/java/ru/yandex/practicum/filmorate/exeption/DirectorNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/DirectorNotFoundException.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.exeption;
 
 public class DirectorNotFoundException extends RuntimeException {
+    public DirectorNotFoundException(int directorId) {
+        super("Режиссер с id" + directorId + " не найден");
+    }
     public DirectorNotFoundException(String message) {
         super(message);
     }
